c79fb88b5dbfdbf113578756ef5739ef45ec2480
aliraza055 <alirazamaqbool055@gmail.com>
2025-10-17 10:55:30 -0700

    add data in items model

--- lib/Models/item_model.dart
+++ lib/Models/item_model.dart
@@ -2,15 +2,15 @@ class Items {
   final String title;
   bool isActive;
   final String image;
-  final bool rating;
+  final double rating;
   final String date;
-  final int price;
+  final String price;
   final String vendor;
-  final String vendorAdress;
+  final String adress;
   final String vendorProfile;
-  final int review;
+  final String review;
   final String bedAndBathroom;
-  final int yearOfHostin;
+  final String yearOfHostin;
   final double latitude;
   final double longitude;
   final List<String> imageUrls;
@@ -22,7 +22,7 @@ class Items {
     required this.date,
     required this.price,
     required this.vendor,
-    required this.vendorAdress,
+    required this.adress,
     required this.vendorProfile,
     required this.review,
     required this.bedAndBathroom,
@@ -31,4 +31,30 @@ class Items {
     required this.longitude,
     required this.imageUrls,
   });
+  static List<Items> itemsData() {
+    return [
+      Items(
+        title: "Nice a bedroom in small nice hosue",
+        isActive: false,
+        image:
+            "https://res.cloudinary.com/dhob4di7g/image/upload/v1760720877/images_2_vjfkkk.jpg",
+        rating: 4.25,
+        date: 'oct-11-2025',
+        price: '2000',
+        vendor: "Saoud",
+        adress: "Lahore, Pakistan",
+        vendorProfile: "vendorProfile",
+        review: "426",
+        bedAndBathroom: "1 bed, shared bathroom",
+        yearOfHostin: '10',
+        latitude: 45.0878,
+        longitude: 4.8585,
+        imageUrls: [
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718520/pexels-jvdm-1454806_1_tm0a3d.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718519/pexels-pixabay-164595_ibv6uh.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718518/pexels-jvdm-2416932_am3r59.jpg',
+        ],
+      ),
+    ];
+  }
 }
