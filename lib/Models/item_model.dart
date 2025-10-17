cf9945c83d9bfc612058240ec342eb6a8845eaa8
aliraza055 <alirazamaqbool055@gmail.com>
2025-10-17 11:17:10 -0700

    complete model data of items

--- lib/Models/item_model.dart
+++ lib/Models/item_model.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:random_string/random_string.dart';
+
 class Items {
   final String title;
   bool isActive;
@@ -31,6 +35,26 @@ class Items {
     required this.longitude,
     required this.imageUrls,
   });
+  Map<String, dynamic> toMap() {
+    return {
+      "title": title,
+      'isActive': isActive,
+      "image": image,
+      "rating": rating,
+      "date": date,
+      "price": price,
+      "vendor": vendor,
+      "address": adress,
+      "vendorProfile": vendorProfile,
+      "review": review,
+      "bedAndBath": bedAndBathroom,
+      "yearOfAutsin": yearOfHostin,
+      "latitue": latitude,
+      "longitude": longitude,
+      "imageUrls": imageUrls,
+    };
+  }
+
   static List<Items> itemsData() {
     return [
       Items(
@@ -55,6 +79,87 @@ class Items {
           'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718518/pexels-jvdm-2416932_am3r59.jpg',
         ],
       ),
+      Items(
+        title: "Luxury apartment with city view",
+        isActive: true,
+        image:
+            "https://res.cloudinary.com/dhob4di7g/image/upload/v1760720877/images_2_vjfkkk.jpg",
+        rating: 4.8,
+        date: 'oct-12-2025',
+        price: '5500',
+        vendor: "Hamza",
+        adress: "Karachi, Pakistan",
+        vendorProfile: "vendorProfile",
+        review: "892",
+        bedAndBathroom: "2 beds, 2 bathrooms",
+        yearOfHostin: '4',
+        latitude: 24.8607,
+        longitude: 67.0011,
+        imageUrls: [
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718445/pexels-naimbic-2029698_jxwm5c.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718486/pexels-burst-545012_hg1hrg.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718496/pexels-izzy-hardy-742982-1591047_o6yzyz.jpg',
+        ],
+      ),
+
+      Items(
+        title: "Peaceful hostel room near university",
+        isActive: true,
+        image:
+            "https://res.cloudinary.com/dhob4di7g/image/upload/v1760720877/images_2_vjfkkk.jpg",
+        rating: 4.2,
+        date: 'oct-09-2025',
+        price: '1200',
+        vendor: "Ali",
+        adress: "Islamabad, Pakistan",
+        vendorProfile: "vendorProfile",
+        review: "315",
+        bedAndBathroom: "1 bed, shared bathroom",
+        yearOfHostin: '2',
+        latitude: 33.6844,
+        longitude: 73.0479,
+        imageUrls: [
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718496/pexels-medhat-ayad-122846-439227_p4onj1.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718499/pexels-pixabay-271618_s2wlmc.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718504/pexels-pixabay-271624_nzpflp.jpg',
+        ],
+      ),
+
+      Items(
+        title: "Beachside resort with private pool",
+        isActive: true,
+        image:
+            "https://res.cloudinary.com/dhob4di7g/image/upload/v1760720877/images_2_vjfkkk.jpg",
+        rating: 4.9,
+        date: 'oct-10-2025',
+        price: '9500',
+        vendor: "Hassan",
+        adress: "Gwadar, Pakistan",
+        vendorProfile: "vendorProfile",
+        review: "1275",
+        bedAndBathroom: "3 beds, 2 bathrooms, pool",
+        yearOfHostin: '6',
+        latitude: 25.1264,
+        longitude: 62.3220,
+        imageUrls: [
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718502/pexels-pixabay-279746_kweakl.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718513/pexels-itsterrymag-2631746_denezj.jpg',
+          'https://res.cloudinary.com/dhob4di7g/image/upload/v1760718515/pexels-pixabay-271619_f3p9fj.jpg',
+        ],
+      ),
     ];
+  }
+}
+
+class SendItemsData {
+  Future<void> sendItemData() async {
+    final items = Items.itemsData();
+    for (var item in items) {
+      String id = randomAlphaNumeric(10);
+      await FirebaseFirestore.instance
+          .collection("ItemsData")
+          .doc(id)
+          .set(item.toMap());
+    }
   }
 }
