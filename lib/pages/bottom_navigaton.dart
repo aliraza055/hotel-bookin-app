0d95ad2f31226a01bc4619e3d4f2effc16a2f0ee
aliraza055 <alirazamaqbool055@gmail.com>
2025-10-17 09:31:21 -0700

    change the color

--- lib/pages/bottom_navigaton.dart
+++ lib/pages/bottom_navigaton.dart
@@ -16,27 +16,31 @@ class _BottomNavigatonState extends State<BottomNavigaton> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[index],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.black,
-        currentIndex: index,
-        //  type: BottomNavigationBarType.fixed,
-        selectedFontSize: 16,
-        //  backgroundColor: Colors.white,
-        onTap: (value) {
-          setState(() {
-            index = value;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'favorite',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'serach'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
-        ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.amber),
+        margin: EdgeInsets.all(12),
+        child: BottomNavigationBar(
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black,
+          currentIndex: index,
+          //type: BottomNavigationBarType.fixed,
+          selectedFontSize: 16,
+          backgroundColor: Colors.amber,
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'favorite',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'serach'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+          ],
+        ),
       ),
     );
   }
