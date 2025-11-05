1a4f022a47477f0fa0a34e12e8a021ad955f7480
aliraza055 <alirazamaqbool055@gmail.com>
2025-11-05 10:34:03 -0800

    add indicators on images

--- lib/pages/favourite_item.dart
+++ lib/pages/favourite_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class FavouriteItem extends StatefulWidget {
   const FavouriteItem({super.key});
@@ -8,6 +9,7 @@ class FavouriteItem extends StatefulWidget {
 }
 
 class _FavouriteItemState extends State<FavouriteItem> {
+  final _controller = PageController();
   final List<String> images = [
     "https://picsum.photos/400/300",
     "https://picsum.photos/400/300",
@@ -25,11 +27,31 @@ class _FavouriteItemState extends State<FavouriteItem> {
             height: 250,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: PageView.builder(
-                itemCount: images.length,
-                itemBuilder: (context, index) {
-                  return Image.network(images[index], fit: BoxFit.cover);
-                },
+              child: Stack(
+                children: [
+                  PageView.builder(
+                    controller: _controller,
+                    itemCount: images.length,
+                    itemBuilder: (context, index) {
+                      return Image.network(images[index], fit: BoxFit.cover);
+                    },
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    left: 100,
+                    right: 0,
+                    child: SmoothPageIndicator(
+                      controller: _controller,
+                      count: images.length,
+                      effect: ExpandingDotsEffect(
+                        activeDotColor: Colors.blue,
+                        dotColor: Colors.white,
+                        dotHeight: 10,
+                        dotWidth: 10,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
